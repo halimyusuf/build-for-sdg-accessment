86ddff100bb0969cc1208afac5c8c6b22e119e11
halim <haleemyoosuph@gmail.com>
2020-04-09 17:59:19 +0100

    initial commit

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,55 @@
-const covid19ImpactEstimator = (data) => data;
+/* eslint-disable no-param-reassign */
+const infectedAtTime = (currentlyInfected, periodType, timeToElapse) => {
+  if (periodType === 'months') {
+    timeToElapse *= 30;
+  } else if (periodType === 'weeks') {
+    timeToElapse *= 7;
+  }
+  const factorDoubles = 2 ** Math.floor(timeToElapse / 3);
+  return currentlyInfected * factorDoubles;
+};
+
+const estimatorHelper = (data, val) => {
+  const infected = data.reportedCases * val;
+  const infectionsByRequestedTime = infectedAtTime(
+    infected,
+    data.periodType,
+    data.timeToElapse
+  );
+  const severeCasesByRequestedTime = Math.floor(
+    0.15 * infectionsByRequestedTime
+  );
+  const hospitalBedsByRequestedTime = Math.floor(
+    data.totalHospitalBeds * 0.35 - severeCasesByRequestedTime
+  );
+  const casesForICUByRequestedTime = Math.floor(
+    severeCasesByRequestedTime * 0.05
+  );
+
+  const casesForVentilatorsByRequestedTime = Math.floor(
+    severeCasesByRequestedTime * 0.02
+  );
+
+  const dollarsInFlight =
+    infectionsByRequestedTime *
+    data.region.avgDailyIncomeInUSD *
+    data.region.avgDailyIncomePopulation *
+    30;
+  return {
+    currentlyInfected: infected,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
+  };
+};
+
+const covid19ImpactEstimator = (data) => ({
+  data,
+  impact: estimatorHelper(data, 10),
+  severeImpact: estimatorHelper(data, 50)
+});
 
 export default covid19ImpactEstimator;
